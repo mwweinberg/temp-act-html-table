fad9ec39f90cb7037db01733316d59de8cf721be
Michael Weinberg <mwweinberg@users.noreply.github.com>
2017-12-31 13:24:39 -0500

    creates archive output, removed troubleshooting

--- temp-act-html-table.py
+++ temp-act-html-table.py
@@ -1,5 +1,9 @@
+#python3
+
 import pandas as pd
 import numpy as np
+
+import time
 
 #load the csv as the dataframe with only the columns you care about
 df = pd.read_csv('input.csv', usecols = ['Organization Name (English)', 'Organization Name (Chinese)', 'Organization Origin', 'Chinese Partner Unit (English)', 'Chinese Partner Unit (Chinese)', 'Activity Name (English)', 'Activity Name (Chinese)', 'Activity Location (English)', 'Activity Location (Chinese)', 'Start Date', 'End Date'])
@@ -12,13 +16,8 @@ df = df.reindex(columns=columnsTitles)
 #prevents truncation of field data
 pd.set_option('max_colwidth', -1)
 
-df.style.render()
-
-#print(df)
-
 #outputs html to a file
 #index = false is to avoid printing the index column
-#df.to_html('output.html', index = False, classes = ['tableizer-firstrow'], border = 0)
 new_table = df.to_html(index = False, classes = ['tableizer-firstrow'], border = 0)
 
 #removes the formatting
@@ -37,27 +36,36 @@ fixed_table_10 = fixed_table_9.replace('<th>Activity Location (Chinese)</th>', '
 fixed_table_11 = fixed_table_10.replace('<th>Start Date</th>', '<th style="width:5%">Start Date</th>')
 fixed_table_12 = fixed_table_11.replace('<th>End Date</th>', '<th style="width:5%">End Date</th>')
 
-#add the bits at the end of the html
-#could do this with a seperate file but it is small enough that this seems easier
-
-#fixed_table_13 = fixed_table_12.replace('</table>', '</table> <!–– add your tableizer data above this line––> </body> </html>')
-
 fixed_table = fixed_table_12
 
-print(fixed_table)
-
+#This is the first part of the html page
 top_half = open('site_top.html', 'r')
 
-output_page = open('tempacttable.html', 'w')
+#this is the regular output
+output_page = open('index_TA_table.html', 'w')
 
+#this creates the archive output
+timestamp = time.strftime("%Y%m%d")
+output_page_archive_filename = "index_TA_table" + timestamp + ".html"
+archive_page = open(output_page_archive_filename, 'w')
 
+#####for the regular page#######
+#writes the html to the new page
 for item in top_half:
     output_page.write(item)
-
+#writes the table
 for item in fixed_table:
     output_page.write(item)
-
+#closes out the page (this should be in the table but it isn't because I don't know why
 output_page.write("table> <!–– add your tableizer data above this line––> </body> </html>")
+
+#####for the archive page#######
+for item in top_half:
+    archive_page.write(item)
+for item in fixed_table:
+    archive_page.write(item)
+archive_page.write("table> <!–– add your tableizer data above this line––> </body> </html>")
 
 top_half.close()
 output_page.close()
+archive_page.close()
